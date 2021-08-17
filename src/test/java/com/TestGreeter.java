eb04c91a8801bd00b0e6efb9c6dbf781a46199d9
rudrakshi139 <71543475+rudrakshi139@users.noreply.github.com>
2021-08-17 14:19:42 +0530

    Update TestGreeter.java

--- src/test/java/com/TestGreeter.java
+++ src/test/java/com/TestGreeter.java
@@ -33,13 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
-  @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you all";
+    String someone = "World, We all know";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
